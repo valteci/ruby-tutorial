0ec9c05da31e633a8f558a706cb0317039a34aef
valteci <valtecijunior@gmail.com>
2024-10-10 13:29:29 -0300

    adicionando um .gitigonre

--- loja_virtual/lib/biblioteca.rb
+++ loja_virtual/lib/biblioteca.rb
@@ -1,26 +1,27 @@
 require File.expand_path('loja_virtual/lib/banco_de_arquivos.rb')
 
 class Biblioteca
-  
+
+  attr_reader :livros
+
   def initialize
-    @livros = {}
+    @livros = []
     @banco_de_arquivos = BancoDeArquivos.new
   end
 
   def adicionar livro
-    @livros[livro.categoria] ||= []
-    @livros[livro.categoria] << livro
-    @banco_de_arquivos.salvar (livro)
-  end
-
-  def livros
-    @livros.values.flatten
+    salvar_livro livro
+    @livros << livro
   end
 
   def livros_por_categoria(categoria)
-    @livros[categoria].each do |livro_meu|
-      yield livro_meu if block_given?
-    end
+    @livros.select { |livro| livro.categoria == categoria }
+  end
+
+  private()
+  def salvar_livro(livro)
+    @banco_de_arquivos.salvar(livro)
+    yield if block_given?
   end
 
 end
--- main.rb
+++ main.rb
@@ -18,8 +18,22 @@ livro2 = Livro.new(
     :banco_de_dados
 )
 
+livro3 = Livro.new(
+    'jose',
+    'Dominando o oracle',
+    '9384',
+    5000,
+    99.33,
+    :banco_de_dados
+)
+
+
 lib = Biblioteca.new
 lib.adicionar livro1
 lib.adicionar livro2
+lib.adicionar livro3
+
+puts lib.livros_por_categoria :banco_de_dados
+
 
 
