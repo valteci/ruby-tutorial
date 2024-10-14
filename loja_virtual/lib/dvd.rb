f980f9034a16e4f5a9d2e78441bcf116cfb9d5a0
valteci <valtecijunior@gmail.com>
2024-10-14 12:58:54 -0300

    criando novos arquivos

--- loja_virtual/lib/dvd.rb
+++ loja_virtual/lib/dvd.rb
@@ -1,3 +1,16 @@
-class Dvd
+class Dvd < Midia
+  attr_accessor :valor
+  attr_reader :titulo
+
+  def initialize(titulo, valor, categoria)
+    @titulo = titulo
+    @valor = valor
+    @categoria = categoria
+  end
+
+  def to_s
+    %Q{Título: #{@titulo}, Valor: #{@valor}}
+  end
   
+
 end
